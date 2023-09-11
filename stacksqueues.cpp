2f580fead34294adc91cbcae50cb411de223c6ef
Akash Kumar Laha <99177461+MeAkash05@users.noreply.github.com>
2023-09-11 22:29:13 +0530

    Add files via upload

---
+++ stacksqueues.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //Stack ->>last in first out
+    stack<int>s;
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+
+
+    //queue
+    queue<string>q;
+    q.push("abc");
+    q.push("def");
+    q.push("ijk");
+    q.push("klm");
+    while(!q.empty()){
+        cout<<q.front()<<endl;
+        q.pop();
+    }
+}
